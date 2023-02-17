96ca28e06f760056ecb79cf4b30913b90c018189
Cynthia Dodzi <dahesey@gmail.com>
2023-02-17 00:32:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@
  * Return: Always 0 (Successs)
  */
 int main(void)
-
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
