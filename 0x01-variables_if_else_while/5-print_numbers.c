80bd89740ac627173319eb44b241c8a5de50c272
dahesey <dahesey@gmail.com>
2023-02-19 16:02:08 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include  <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int a = 0;
+	while (1 < 10)
+	{
+		printf("%d",a);
+		a++;
+	}
+	putchar('\a');
+
+	return (0);
+}
