5b98f291d5a941fff44625eea7f95c99b714b3c4
dahesey <dahesey@gmail.com>
2023-02-23 15:35:16 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * check lower case - check for lower case
+ *
+ * Return: 1 if lower or 0 if otherwise
+ */
+
+int _islower(int c);
+{
+	if( c >= 96 && c<= 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
