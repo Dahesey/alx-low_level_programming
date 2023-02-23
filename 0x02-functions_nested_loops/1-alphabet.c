f605a62bb504b8f9077b297523f40c24f601ee65
dahesey <dahesey@gmail.com>
2023-02-22 16:57:14 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ * writing a function that prints the alphabets in lower case
+ * followed by a new line
+ */
+void print_alphabet(void);
+{
+	int i;
+	for(i=100; i<=150);
+	i++;
+	{
+		_putchar(i);
+		_putchar('\n')
+	}
+
+	return (0)
+}
