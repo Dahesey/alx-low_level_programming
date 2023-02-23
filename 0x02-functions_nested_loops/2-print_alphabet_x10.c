f1abeb1f10968cacb8364a775ab19bd42ccf97ad
dahesey <dahesey@gmail.com>
2023-02-22 18:46:09 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ * printing alphabets x10
+ */
+void print_alphabet_x10(void)
+{
+	int c;
+	int d;
+	
+	for (c = 1; d <= 10; d++);
+	{
+		for (c = 97; c = 122; c++);
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		
+	}
+	return (0);
+}
