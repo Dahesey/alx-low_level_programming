60428182255e021bac57d094aac3c0c6c9862069
dahesey <dahesey@gmail.com>
2023-02-22 17:11:05 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,18 +4,16 @@
  *
  * Return: 0 (Success)
  *
- * writing a function that prints the alphabets in lower case
- * followed by a new line
+ * writing a function that prints the alphabets in lower case 
+ * and add a new line
  */
+
 void print_alphabet(void);
 {
 	int i;
-	for(i=100; i<=150);
-	i++;
+	for(i=97; i<=122,i++);
 	{
 		_putchar(i);
-		_putchar('\n')
 	}
-
-	return (0)
-}
+	_putchar('\n');
+		return (0);
