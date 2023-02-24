86cdba6a00015d84432dbc577de47d19e05711d5
dahesey <dahesey@gmail.com>
2023-02-24 07:11:19 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,14 @@
 
 /**
  * mul - multiplies 2 integers
- * @a: represent integer
- * @b: represent integer
+ * @a: represent first integer
+ * @b: represent second integer
  * Return: 0 (Success)
  */
 int mul(int a, int b)
 {
 	int result;
+
 	result = a * b;
 		return(result);
 }
