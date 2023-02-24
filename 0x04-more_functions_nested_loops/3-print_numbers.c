c625fc32e2b18065d37e3218d67e78622995746f
dahesey <dahesey@gmail.com>
2023-02-24 09:10:18 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 10
+ *
+ * Return: 0 (Success)
+ */
+void print_numbers()
+{
+	int i = 48;
+	
+	while (i <= 57);
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
