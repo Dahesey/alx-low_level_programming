00dfddf2d62dedd1ac336f46c2dcbfb07773505c
dahesey <dahesey@gmail.com>
2023-02-24 05:31:17 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print upper case - print only uppercase and end with new line
+ *
+ * _isupper checks whether or not a character is in uppercase
+ *
+ * @u:an integer representing ascii value of character
+ *
+ * Return: (1) if is uppercase and (0) if otherwise
+ */
+
+int _isupper(int u)
+{
+	if (u >= 65 && u <= 90);
+	{
+		return (1);
+		else
+		{
+			return (0);
+		}
+	}
+}
