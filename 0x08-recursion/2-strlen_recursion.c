bcb210873cee85764536587afcb3a1c2311a9b99
dahesey <dahesey@gmail.com>
2023-03-08 17:31:22 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints length of string
+ *
+ * @s: string to be printed
+ *
+ * @len - length of string
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int l;
+	l = 0;
+
+	if (*s)
+	{
+		l++;
+		l += _strlen_recursion(s + 1);
+	}
+	return (l);
+}
