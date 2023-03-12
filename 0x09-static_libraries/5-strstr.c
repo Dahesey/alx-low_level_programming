534feeca474ccc663296992d7487551070fc09f3
dahesey <dahesey@gmail.com>
2023-03-12 16:37:27 -0700

    libmy.a

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - function finds the first occurrence of
+ * the substring needle in the string haystack
+ *
+ * @haystack: character string
+ *
+ * @needle: character substring
+ *
+ * Return: 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
