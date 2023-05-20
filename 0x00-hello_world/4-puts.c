208ad595bd2610a13d03630787af3023d1ebfa60
Dahesey <dahesey@gmail.com>
2023-05-19 18:36:56 -0700

    Printing a string with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * puts - prints strings
+ *
+ * char: string
+ *
+ * Return: (0)
+ */
+
+#include <stdio.h>
+char main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
