1afd942692a353c6eedae4af3d6b085af563f286
Dahesey <dahesey@gmail.com>
2023-05-19 20:48:11 -0700

    Corrected string

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -22,7 +22,7 @@ int main(void)
 	printf("size of a char: %lu byte(s)\n", sizeof(c));
 	printf("size of a float: %lu byte(s)\n", sizeof(f));
 	printf("size of a long int: %lu byte(s)\n", sizeof(li));
-	printf("size of a long long int %lu byte(s)\n", sizeof(lli));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(lli));
 
 	return (0);
 }
