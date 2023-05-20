37fd5ac2c690e0b2caaf90ae466d9e30550bf0e5
Dahesey <dahesey@gmail.com>
2023-05-19 18:55:54 -0700

    printing a string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point
+ *
+ * printf - prints strings
+ *
+ * Return: (0)
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+}
