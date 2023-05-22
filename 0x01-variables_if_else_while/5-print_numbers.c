4243bfe36c397bc25a37dab2c90735358d78acdf
Dahesey <dahesey@gmail.com>
2023-05-22 16:34:30 -0700

    A program to print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/**
+ * main - Entry point
+ *
+ * printf - prints digit and char
+ *
+ * Author - Dahesey
+ *
+ * Return: (0)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+	printf("%d\n",i++);
+	}
+
+	return (0);
+}
