5febd0dabd13e23583b8fc978e1aebb2fbdb75e7
Dahesey <dahesey@gmail.com>
2023-05-23 09:08:24 -0700

    A program to print double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+/**
+ * main - Entry point
+ *
+ * putchar - prints char and strings
+ *
+ * Author - Dahesey
+ *
+ * Return: (0)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int q = 48;
+	int p = 48;
+
+	while (q < 58)
+	{
+		p = 48;
+		while (p < 58)
+		{
+			if (q != p && q < p)
+			{
+				putchar(q);
+				putchar(p);
+				if (!(q == 56 && p == 57))
+				{
+					putchar('.');
+					putchar(' ');
+				}
+			}
+			p++;
+		}
+		q++;
+	}
+	putchar('\n');
+	return (0);
+}
