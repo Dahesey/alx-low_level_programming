62bbf805185fad4e8cf9b6abd2907931420f06e5
Dahesey <dahesey@gmail.com>
2023-05-26 10:58:08 -0700

    Print squares using hash sign

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+/**
+ * print_square - prints squares using hash sign
+ *
+ * Author - Dahesey
+ *
+ * @size: size of the square
+ */
+
+#include "main.h"
+
+void print_square(int size)
+{
+	int sq = 0, y;
+	
+	if (size > 0)
+	{
+		while (sq < size)
+		{
+			y = 0;
+			while (y < size)
+			{
+				_putchar(35);
+				y++;
+			}
+			_putchar('\n');
+			sq++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
