e9fc4dd8a8b946b7dadef458e18556af9be63109
Dahesey <dahesey@gmail.com>
2023-05-25 18:43:14 -0700

    A function to multoply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+/**
+ * mul - multiplies two digits
+ *
+ * @a: first integer to be multiplied
+ *
+ * @b: second integer to be multiplied
+ *
+ * Author - Dahesey
+ *
+ * Return: (0)
+ */
+
+#include "main.h"
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a*b;
+
+	return (res);
+}
