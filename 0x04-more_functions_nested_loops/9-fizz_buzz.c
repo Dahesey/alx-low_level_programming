2423c61fb1f76911e37ba196c726d7b210f77eab
Dahesey <dahesey@gmail.com>
2023-05-28 05:25:00 -0700

    A program to print fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+/**
+ * main - Entry point
+ *
+ * Author - Dahesey
+ *
+ * Return: (0)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int f = 2;
+	int x = 1;
+
+	printf("%d ", x);
+
+	while (f <= 100)
+	{
+		if (f % 3 == 0 && f % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		else if (f % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (f % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ",f);
+		}
+		f++;
+	}
+	printf("\n");
+
+	return (0);
+}
