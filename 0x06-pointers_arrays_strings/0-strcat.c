c6f1fd763c9de5088172654b6ec5047b9cc47e6d
Dahesey <dahesey@gmail.com>
2023-06-01 16:12:20 -0700

    second string added to first string, main.h file also added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+/**
+ * _strcat - Add first string to second string
+ *
+ * Author - Dahesey
+ *
+ * @dest - first string
+ *
+ * @src - second string
+ *
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int t = 0;
+
+	while (dest[t] != '\0')
+	{
+		t++;
+	}
+	while (*src != '\0')
+	{
+		dest[t] = *src;
+		src++;
+		t++;
+	}
+	dest[t] = '\0';
+	return(dest);
+}
