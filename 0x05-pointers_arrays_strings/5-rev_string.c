698acbb1f980dbfff8ce1d854a53481ea1a21a3c
dahesey <dahesey@gmail.com>
2023-06-03 21:45:26 +0000

    a function to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+/**
+ * rev_string - Reverses string
+ *
+ * Author - Dahesey
+ *
+ * @s: String to be reversed
+ *
+ * Return: void
+ */
+
+#include "main.h"
+
+void rev_string(char *s)
+{
+	int x = 0;
+	int y = 0;
+	char *p;
+
+	for (*p = s; s != '\0'; s++)
+		x++;
+	for (; j < i / 2; j++)
+	{
+		char keep;
+
+		keep = *--s;
+		*p = *s;
+		*p++ = keep;
+	}
+}
