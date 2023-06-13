92bf28f8d1d73f83a41890094552a026981da49a
Dahesey <dahesey@gmail.com>
2023-06-13 04:14:29 -0700

    A program to multiply 2 numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+/**
+ * main - function that prints to stdout
+ *
+ * Author - dahesey
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: (0)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+	int d;
+
+	if (argc > 1)
+	{
+		for (d = 1; d < argc; d++)
+		{
+			mul *= atoi(argv[d]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}
