4035146f78485aa1c576a837de055fdc51f3f6b9
Dahesey <dahesey@gmail.com>
2023-06-13 05:09:46 -0700

    code editted by adding error message and condition

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,7 +7,7 @@
  *
  * @argv: argument vector
  *
- * Return: (0)
+ * Return: (0) on success (1) on error
  */
 
 #include <stdio.h>
@@ -17,7 +17,12 @@ int main(int argc, char *argv[])
 	long int mul = 1;
 	int d;
 
-	if (argc > 1)
+	if (argc > 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	if (argc >= 2)
 	{
 		for (d = 1; d < argc; d++)
 		{
@@ -25,7 +30,5 @@ int main(int argc, char *argv[])
 		}
 		printf("%ld\n", mul);
 	}
-	else
-		printf("Error\n");
 	return (0);
 }
