7c3089c029bedc17f1eb9235b9e38ed12827558a
Dahesey <dahesey@gmail.com>
2023-06-16 07:52:22 -0700

    A function that creates a grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+/**
+ * alloc_grid - returns a pointer to 2 dimentional arrya of integers
+ *
+ * Author - Dahesey
+ *
+ * @width: width of the array
+ *
+ * @height: height of array
+ *
+ * Return: integer pointer
+ */
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int x;
+	int y;
+	int index;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+	a = (int **)malloc(sizeof(int *) * height);
+	if (a == 0)
+	{
+		free(a);
+		return (0);
+	}
+	for (index = 0; index < height; index++)
+	{
+		*(a + index) = (int *)malloc(sizeof(int *) * width);
+		if (*(a + index) == 0)
+		{
+			for (x = 0; x < index; x++)
+			free(*(a + index));
+			free(a);
+			return (0);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			*(*(a + x) + y) = 0;
+	}
+	return (a);
+}
