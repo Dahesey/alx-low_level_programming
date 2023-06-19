c248b116e6005efbbe096ee7b58e7d9dcf9cb90e
Dahesey <dahesey@gmail.com>
2023-06-19 15:03:40 -0700

    Added conditional statements

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,11 +10,17 @@
 #include <stdio.h>
 void print_dog(struct dog *d)
 {
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %1f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	if (d == NULL)
+	if (d->age < 0)
 		printf("(nil)\n");
-	printf("Name: %s\n", d->name);
-	printf("Age: %1f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("(nil)\n");
 }
