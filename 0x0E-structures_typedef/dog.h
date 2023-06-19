786cc60bfa9f6f5c2245a860f0a2b932415ead03
Dahesey <dahesey@gmail.com>
2023-06-19 09:21:36 -0700

    A header file dog.h was defined

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - data tag
+ *
+ * Author - Dahesey
+ *
+ * @name: First element of structure
+ *
+ * @age: Second element of structure
+ *
+ * @owner: Third element of structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H */
