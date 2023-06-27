64ebb2e3b35434d3ffc7cc9643dc83ccec1ad8fb
Dahesey <dahesey@gmail.com>
2023-06-27 04:17:07 -0700

    A recursive function to print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * _puts_recursion - Function that prints a  string
+ *
+ * Author - Dahesey
+ *
+ * @s: char
+ */
+#include "main.h"
+void _puts_recursion(char *s)
+{
+	char *y = s;
+	
+	if (*y)
+	{
+		_putchar(*y);
+		_puts_recursion(y + 1);
+	}
+	else
+		_putchar('\n');
+}
