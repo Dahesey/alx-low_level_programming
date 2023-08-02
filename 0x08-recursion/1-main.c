8d91cc54d3d9fa222602f4761c55812eb2892f96
Dahesey <dahesey@gmail.com>
2023-08-02 17:48:09 +0000

    Print a string in reverse using recursion

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,19 @@
+/**
+ * _print_rev_recursion - A function to print a string in reverse using recursion
+ *
+ * Author - Dahesey
+ */
+
+
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _print_rev_recursion - A function to print a string using recursion
+ *
+ * Author - Dahesey
+ *
+ * @s: char string to be printed
+ */
+
+#include "main.h"
+void _print_rev_recursion(char *s)
+{
+	char *p = s;
+	
+	if(*p != '\0')
+	{
+		_print_rev_recursion(p + 1);
+		_putchar(*p);
+	}
+}
