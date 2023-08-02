f4d1988e56ef006d06e7948b2d38218167f8a669
Dahesey <dahesey@gmail.com>
2023-08-02 22:13:31 +0000

    Factorial of a number

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen_recursion - A function that returns length of string
+ *
+ * Author - Dahesey
+ *
+ * @s: char string to be printed
+ *
+ * Return: length of string
+ */
+
+#include "main.h"
+int _strlen_recursion(char *s)
+{
+	char *p = s;
+
+	if(!*p)
+		return (0);
+	else
+		return (1 + _strlen_recursion(p + 1));
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/**
+ * factorial - A function that finds the factorial of a number
+ *
+ * Author - Dahesey
+ *
+ * @n: integer to be checked
+ *
+ * Return: -1 if n is lower than 0 to indicate an error 
+ * 	  1 if n is 0
+ */
+
+#include "main.h"
+int factorial(int n)
+{
+	if(n < 0)
+		return (-1);
+	if(n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
