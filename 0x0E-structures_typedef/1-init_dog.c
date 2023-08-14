d9c552b2d5371379f346a5a86d756668df8f4cf1
dahesey <dahesey@gmail.com>
2023-08-14 13:54:43 +0000

    Added braces

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,11 +10,14 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+#include "main.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}
 }
