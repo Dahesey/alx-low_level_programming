8acfdd94a1eb8f1fb8ebedef04c2fc2c29b2e282
dahesey <dahesey@gmail.com>
2023-08-14 12:49:09 +0000

    Added a dog.h file for task 1 defining  a new structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+  * struct dog - Type and name of structure containing different data types
+  * Author - Dahesey
+  * @name: char data type
+  * @age: float data type
+  * @owner: char data type
+  */
+
+#include <stdio.h>
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
