1b9f91dc9c787b3a00f31597b6fc2c88a8119e1e
dahesey <dahesey@gmail.com>
2023-08-18 02:59:03 +0000

    Added a header to the header file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,9 +4,7 @@
   * @separator: A string that separates the numbers to be printed
   * @n: last mandatory integer
   */
-#include <stdio.h>
 #include "variadic_functions.h"
-#include <stdarg.h>
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int a;
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,11 @@
 #define VARIADIC_FUNCTION_H
 
 int _putchar(char c);
+#include <stdio.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
