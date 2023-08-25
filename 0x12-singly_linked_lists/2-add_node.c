59d57caca548f58b53b7733b6264efb1e96f7b3b
dahesey <dahesey@gmail.com>
2023-08-25 13:44:15 +0000

    A function to add nodes to a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+/**
+  * add_node - A function to add nodes to a linked list
+  * @head: The head of the list that points to a pointer
+  * @str: The pointer string to be added
+  * Return: AA pointer to the new node added or NULL if fail
+  */
+#include "lists.h"
+list_t *add_node(list_t **head, const char *s)
+{
+	list_t **pointer;
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (s == NULL)
+		s = "";
+	new_node->str = strdup(s);
+	new_node->len = strlen(s);
+	
+	for (pointer = head; *pointer != NULL; pointer = &(**pointer).next)
+		;
+	*pointer = new_node;
+	new_node->next = NULL;
+	return (new_node);
+}
