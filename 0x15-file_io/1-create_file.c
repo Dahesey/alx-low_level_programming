939214f62edd482149dccf2ccced11dd54ebf483
dahesey <dahesey@gmail.com>
2023-09-07 14:10:49 +0000

    A function that creates a file that can only be accessed by the owner

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+/**
+  * create_file - A function that creates a file
+  * @filename: Name of file
+  * @text_content: a NULL terminated string to write to the file
+  * Return: 1 on success and -1 on failure
+  */
+#include "main.h"
+int create_file(const char *filename, char *text_content)
+{
+	int descript, chars, wr;
+
+	if (!filename)
+		return (-1);
+	descript = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	if (descript == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (chars = 0; text_content[chars]; chars++)
+		;
+
+	wr = write(descript, text_content, chars);
+	if (wr == -1)
+		return (-1);
+	close(descript);
+	return (1);
+}
