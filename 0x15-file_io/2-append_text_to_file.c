a8b037663f75f8bcb2a4eb17615ead62a40e7d8b
dahesey <dahesey@gmail.com>
2023-09-08 15:29:34 +0000

    Changed a line of code to see if it will work

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,19 +10,24 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int descript, chars, wr;
 
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content= "";
+
 	if (filename)
 	{
-		descript = open(filename, O_WRONLY | O_APPEND);
-		if (descript == -1)
+		descript = open(filename, O_APPEND | O_WRONLY);
+		if (descript < 0)
 			return (-1);
-		if (text_content)
-		{
-			for (chars = 0; text_content[chars]; chars++)
-				;
-			wr = write(descript, text_content, chars);
-			if (wr == -1)
-				return (-1);
-		}
+	}
+	if (text_content)
+	{
+		for (chars = 0; text_content[chars]; chars++)
+			;
+		wr = write(descript, text_content, chars);
+		if (wr == -1)
+			return (-1);
 	}
 	close(descript);
 
