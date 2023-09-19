19b31bb53f981f232dfc5dc0e14be876136b87b7
Dahesey <dahesey@gmail.com>
2023-09-19 15:21:18 +0000

    A function to convert binary to an unsigned  number

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdlib.h>
+#include <stdio.h> 
+
+#define FALSE 0
+#define BIN 2
+
+unsigned long int get_pow(int x, int y)
+unsigned int binary_to_uint(const char *b)
+
+#endif /** MAIN_H **/
