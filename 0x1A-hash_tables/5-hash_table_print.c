42e97fa74d8630c13727f3df91f6d37d352f1cc5
Dahesey <dahesey@gmail.com>
2023-11-06 12:39:20 +0000

    A function to print the keys and values in a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+/**
+ * hash_table_print - A function to print a hash table
+ *
+ * AUthor - Dahesey
+ *
+ * @ht: The hash table to be printed
+ *
+ * Return: hash table key and value, nothing if hashtable is NULL
+ */
+
+#include "hash_tables.h"
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int a;
+	int print = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (a = 0; a < ht->size; a++)
+	{
+		hash_node_t *current = ht->array[a];
+		while (current != NULL)
+		{
+			if (print)
+				printf(",");
+			printf("'%s': '%s'", current->key, current->value);
+			print = 1;
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
